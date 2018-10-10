b0577c1aac41d9e72fbd9647e5e036f60439db9a
Hamza Bentebbaa <hbentebbaa@gmail.com>
2018-10-09 22:40:59 -0400

    linting - consistency

--- mobileApp/screens/AuthLoadingScreen.js
+++ mobileApp/screens/AuthLoadingScreen.js
@@ -1,34 +1,35 @@
 import React from 'react';
-import {ActivityIndicator, AsyncStorage, StatusBar, StyleSheet, View} from 'react-native';
-
-export default class AuthLoadingScreen extends React.Component {
-    constructor(props) {
-        super(props);
-        this._bootstrapAsync();
-    }
-
-    // Fetch the token from storage then navigate to our appropriate place
-    _bootstrapAsync = async() => {
-        const accessToken = await AsyncStorage.getItem('accessToken');
-
-        this.props.navigation.navigate(accessToken ? 'Main' : 'Auth');
-    };
-
-    // Render any loading content that you like here
-    render() {
-        return (
-            <View style={styles.container}>
-                <ActivityIndicator/>
-                <StatusBar barStyle="default"/>
-            </View>
-        );
-    }
-}
+import { ActivityIndicator, AsyncStorage, StatusBar, StyleSheet, View } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
+
+export default class AuthLoadingScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this._bootstrapAsync();
+  }
+
+  // Fetch the token from storage then navigate to our appropriate place
+  _bootstrapAsync = async () => {
+    const { navigation } = this.props;
+
+    const accessToken = await AsyncStorage.getItem('accessToken');
+    navigation.navigate(accessToken ? 'Main' : 'Auth');
+  };
+
+  // Render any loading content that you like here
+  render() {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+        <StatusBar barStyle="default" />
+      </View>
+    );
+  }
+}
--- mobileApp/screens/SignInScreen.js
+++ mobileApp/screens/SignInScreen.js
@@ -3,45 +3,46 @@ import { Button, View, Text, StyleSheet, AsyncStorage } from 'react-native';
 
 import { signInWithGoogleAsync } from '../helpers/Login';
 
-export default class SignInScreen extends React.Component {
-    static navigationOptions = {
-        title: 'Please sign in',
-    };
-
-    state = {
-        loginFailed: false,
-    }
-
-    render() {
-        const {loginFailed} = this.state;
-
-        return (
-            <View style={styles.container}>
-                {loginFailed && <Text>LOGIN GAILED!!!</Text>}
-                <View>THIS IS SIGN IN SCREEN</View>
-                <Button title="Sign in!" onPress={this._signInAsync} />
-            </View>
-        );
-    }
-
-    _signInAsync = async () => {
-        const result = await signInWithGoogleAsync();
-
-        if (result.accessToken) {
-            await AsyncStorage.setItem('accessToken', result.accessToken);
-            this.props.navigation.navigate('Main');
-        }
-
-        this.setState({
-            loginFailed: true,
-        });
-    };
-}
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
+
+export default class SignInScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Please sign in',
+  };
+
+  state = {
+    loginFailed: false,
+  };
+
+  render() {
+    const { loginFailed } = this.state;
+
+    return (
+      <View style={styles.container}>
+        {loginFailed && <Text>LOGIN GAILED!!!</Text>}
+        <View>THIS IS SIGN IN SCREEN</View>
+        <Button title="Sign in!" onPress={this._signInAsync} />
+      </View>
+    );
+  }
+
+  _signInAsync = async () => {
+    const { navigation } = this.props;
+    const result = await signInWithGoogleAsync();
+
+    if (result.accessToken) {
+      await AsyncStorage.setItem('accessToken', result.accessToken);
+      navigation.navigate('Main');
+    }
+
+    this.setState({
+      loginFailed: true,
+    });
+  };
+}
