5caf7d03dea8914a70ede2608bb44b89101be87f
Hamza Bentebbaa <hbentebbaa@gmail.com>
2018-10-21 23:00:34 -0400

    Api calls

---
+++ mobileApp/api/user.js
@@ -0,0 +1,16 @@
+/* global __DEV__ */
+import axios from 'axios';
+
+const apiPath = __DEV__ ? 'http://localhost:3000' : '_FILL_IN_PROD_API_PATH';
+
+export const getUser = async userId =>
+  axios
+    .get(`${apiPath}/users/${userId}`)
+    .then(res => ({
+      success: true,
+      user: res.data,
+    }))
+    .catch(err => ({
+      success: false,
+      error: err,
+    }));
