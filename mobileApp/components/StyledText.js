6a4fffa5f3e0836989a02ba54fec75bbec2b3baf
Hamza Bentebbaa <hbentebbaa@gmail.com>
2018-10-21 22:59:44 -0400

    merge styles correctly

--- mobileApp/components/StyledText.js
+++ mobileApp/components/StyledText.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 import { primaryFontColor } from '../styles/Constants';
 
 /* eslint-disable react/destructuring-assignment */
 export const RegularText = props => (
   <Text
     {...props}
-    style={[{ color: primaryFontColor, fontFamily: 'Roboto-Regular' }, props.style]}
+    style={StyleSheet.flatten([
+      {
+        color: primaryFontColor,
+        fontFamily: 'Roboto-Regular',
+      },
+      props.style,
+    ])}
   />
 );
 
 export const BoldText = props => (
-  <Text {...props} style={[props.style, { color: primaryFontColor, fontFamily: 'Roboto-Bold' }]} />
+  <Text
+    {...props}
+    style={StyleSheet.flatten([
+      {
+        color: primaryFontColor,
+        fontFamily: 'Roboto-Bold',
+      },
+      props.style,
+    ])}
+  />
 );
 
 export const MutedSmallText = props => (
