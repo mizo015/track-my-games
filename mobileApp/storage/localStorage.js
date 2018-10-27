840975001ebd82e050c689154d8afa67bce5bd56
Hamza Bentebbaa <hbentebbaa@gmail.com>
2018-10-26 21:47:39 -0400

    handle parsing

--- mobileApp/storage/localStorage.js
+++ mobileApp/storage/localStorage.js
@@ -1,6 +1,6 @@
 import { AsyncStorage } from 'react-native';
 
-export const getItem = async key => AsyncStorage.getItem(key);
+export const getItem = async key => JSON.parse(await AsyncStorage.getItem(key));
 
 export const setItem = async (key, value) => AsyncStorage.setItem(key, JSON.stringify(value));
 
