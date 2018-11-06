89a3408d4eccf2d04fce7f0e2f58e5f0e5290829
Hamza Bentebbaa <hbentebbaa@gmail.com>
2018-11-06 11:38:07 -0500

    consistent headers

--- mobileApp/components/Header.js
+++ mobileApp/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Proptypes from 'prop-types';
-import { Container, Header as NBHeader, Left, Body, Right, Button, Title } from 'native-base';
+import { Header as NBHeader, Left, Body, Right, Button, Title } from 'native-base';
 import { withNavigation } from 'react-navigation';
 
 import { IconEntypo } from './Icons';
@@ -8,27 +8,26 @@ import { styles } from '../styles/App';
 import { primaryColor } from '../styles/Constants';
 
 const Header = props => {
-  const { title } = props;
+  const { title, refresh } = props;
 
   return (
-    <Container style={styles.headerContainer}>
-      <NBHeader style={styles.header}>
-        <Left />
-        <Body>
-          <Title>{title}</Title>
-        </Body>
-        <Right>
-          <Button transparent onPress={() => props.navigation.navigate('NewGame')}>
-            <IconEntypo name="add-to-list" size={25} color={primaryColor} />
-          </Button>
-        </Right>
-      </NBHeader>
-    </Container>
+    <NBHeader style={styles.header}>
+      <Left />
+      <Body>
+        <Title>{title}</Title>
+      </Body>
+      <Right>
+        <Button transparent onPress={() => props.navigation.navigate('NewGame', { refresh })}>
+          <IconEntypo name="add-to-list" size={25} color={primaryColor} />
+        </Button>
+      </Right>
+    </NBHeader>
   );
 };
 
 Header.propTypes = {
   title: Proptypes.string.isRequired,
+  refresh: Proptypes.func.isRequired,
 };
 
 export default withNavigation(Header);
